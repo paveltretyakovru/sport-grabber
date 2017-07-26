09809ce4cdfc94a78414a3ceae374dd2754e85f5
Pavel <tretyakovpavel.ru@gmail.com>
2017-07-26 21:34:09 +0300

    Prepeared index page temp layout

--- src/frontend/app/pages/index/index.container.js
+++ src/frontend/app/pages/index/index.container.js
@@ -5,20 +5,72 @@ import { bindActionCreators } from 'redux';
 import * as AppActions from 'app/app.actions';
 import * as HeaderActions from 'app/shared/header/header.actions';
 
+// Material-ui components
+import IconButton from 'material-ui/IconButton';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import {GridList, GridTile} from 'material-ui/GridList';
+
+const tilesData = [
+  {
+    img: 'https://sportivnye-prognozy.ru/wp-content/uploads/2017/07/xmajer-kuznetsov-200x200.png.pagespeed.ic.xnTENWx3wn.webp',
+    title: 'Суареш-Наварро — Мертенс. 27.07.2017',
+    author: 'jill111',
+    featured: true,
+  },
+  {
+    img: 'https://sportivnye-prognozy.ru/wp-content/uploads/2017/07/xsuaresh-navarro-mertens-200x200.png.pagespeed.ic.iPPcJyMMuy.webp',
+    title: 'Севастова — Куличкова. 26.07.2017',
+    author: 'pashminu',
+    featured: true,    
+  },
+  {
+    img: 'https://sportivnye-prognozy.ru/wp-content/uploads/2017/07/xbarcelona-200x200.png.pagespeed.ic.SihhFFHBnx.webp',
+    title: 'Майер — Кузнецов. 26.07.2017',
+    author: 'Danson67',
+  },
+];
+
+const styles = {
+  gridTile: {
+    marginTop: 16,
+    marginBottom: 16,
+  },
+  gridTileTitle: {
+    fontSize: '13px',
+  },
+  gridImage: {
+    width: '100%',
+  },
+};
+
 export class IndexContainer extends Component {
   componentWillMount() {
     if(this.props.setHeaderButtons !== undefined) {
       this.props.setHeaderButtons(null, null);
-      this.props.headerActions.updateHeaderTitle('React application');
+      this.props.headerActions.updateHeaderTitle('Sport-grabber');
     }
   }
 
   render() {
     return (
-      <div>
-        <h1>Index Container</h1>
-        <input type="text" placeholder="Input 1" defaultValue="Input 1 value" />
-        <button id="change-author-button">Change author name</button>
+      <div className="row center-md center-sm center-xs">
+        <div className="col-md-4 col-sm-5 col-xs-12">
+          <GridList cols={1}>
+            {tilesData.map((tile) => (
+              <GridTile
+                key={tile.img}
+                title={tile.title}
+                style={styles.gridTile}
+                titleStyle={styles.gridTileTitle}
+                actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+                actionPosition="left"
+                titlePosition="top"
+              >
+                <img src={tile.img} style={styles.gridImage} />
+              </GridTile>
+            ))}
+          </GridList>
+        </div>
       </div>
     );
   }
