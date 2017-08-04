ea493aff1f7738df2d7f46670f26878ccc54a7f1
Pavel <tretyakovpavel.ru@gmail.com>
2017-08-04 19:10:12 +0300

    Updated .eslintrc and events.constants.js. Set global SERVER_PORT and SERVER_HOST variables

--- src/frontend/app/pages/events/events.constants.js
+++ src/frontend/app/pages/events/events.constants.js
@@ -1,7 +1,7 @@
 export const SET_EVENTS = 'SET_EVENTS';
 export const EVENTS_ROUTE = 'events';
 export const ADD_EVENTS_PAGE = 'ADD_EVENTS_PAGE';
-export const EVENTS_FETCH_URL = 'http://localhost:3000/events';
+export const EVENTS_FETCH_URL = `http://${SERVER_HOST}:${SERVER_PORT}/events`;
 export const FETCH_EVENT_POST = 'FETCH_EVENT_POST';
 export const CLEAR_CURRENT_POST = 'CLEAR_CURRENT_POST';
 export const ADD_MORE_EVENTS_PAGE = 'ADD_MORE_EVENTS_PAGE';
